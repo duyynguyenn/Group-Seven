3d51ba57ac5129b0611ca4fb6ec87513af6c7de0
ducnam2003 <92658152+ducnam2003@users.noreply.github.com>
2022-10-03 23:01:05 +0700

    update,file product_image,recipients

--- dao/product_image.php
+++ dao/product_image.php
@@ -1,29 +1,29 @@
 <?php
-// lấy nhiều
+// lấy toàn bộ dữ liệu của bảng
 function product_select_all()
 {
   $sql = "SELECT * FROM product_image";
   return  pdo_query($sql);
 }
-//lấy 1
+//lấy 1 bản ghi dựa vào id
 function product_select_by_id($id)
 {
   $sql = "SELECT * FROM product_image Where id = ?";
   return pdo_query_one($sql, $id);
 }
-//thêm
+//thêm dữ liệu vào bảng
 function product_insert($id, $image, $product_id)
 {
   $sql = "INSERT INTO product (id, image, product_id) values(?,?,?)";
   pdo_execute($sql, $id, $image , $product_id);
 }
-//sửa
+//cập nhập dữ liệu dựa vào id
 function product_update($id,$image,$product_id)
 {
   $sql = "UPDATE product_image SET id = ?, image = ? where product_id = ?";
   pdo_execute($sql, $id, $image, $product_id);
 }
-//xoá
+//xoá bản ghi dựa vào id
 function product_delete($id)
 {
   $sql = "DELETE FROM product_image WHERE id = ?";
--- dao/recipients.php
+++ dao/recipients.php
@@ -1,29 +1,29 @@
 <?php
-// lấy nhiều
+// lấy toàn bộ dữ liệu của bảng
 function recipients_select_all()
 {
   $sql = "SELECT * FROM recipients";
   return  pdo_query($sql);
 }
-//lấy 1
+//lấy 1 bản ghi dựa vào id
 function recipients_select_by_id($id)
 {
   $sql = "SELECT * FROM recipients Where id = ?";
   return pdo_query_one($sql, $id);
 }
-//thêm
+//thêm dữ liệu vào bảng
 function recipients_insert($name, $phone, $recipients_id, $user_id)
 {
   $sql = "INSERT INTO recipients (name, phone,recipients_id, user_id) values(?,?,?,?,?)";
   pdo_execute($sql,$name, $phone, $recipients_id, $user_id);
 }
-//sửa
+//cập nhập dữ liệu dựa vào id
 function recipients_update($id, $name, $phone, $recipients_id, $user_id)
 {
   $sql = "UPDATE recipients SET name = ?, price = ?, quantity = ?, featured = ?, active = ?, description = ?, category_id = ?, voucher_id = ?, brand_id = ? where id = ?";
   pdo_execute($sql, $name, $phone, $recipients_id, $user_id, $id);
 }
-//xoá
+//xoá bản ghi dựa vào id
 function recipients_delete($id)
 {
   $sql = "DELETE FROM recipients WHERE id = ?";
