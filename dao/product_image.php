48b7a76ae82596b89975962b71d5308ee558dc3e
ducnam2003 <92658152+ducnam2003@users.noreply.github.com>
2022-10-03 22:57:10 +0700

    tạo dao file product_image,recipients

---
+++ dao/product_image.php
@@ -0,0 +1,36 @@
+<?php
+// lấy nhiều
+function product_select_all()
+{
+  $sql = "SELECT * FROM product_image";
+  return  pdo_query($sql);
+}
+//lấy 1
+function product_select_by_id($id)
+{
+  $sql = "SELECT * FROM product_image Where id = ?";
+  return pdo_query_one($sql, $id);
+}
+//thêm
+function product_insert($id, $image, $product_id)
+{
+  $sql = "INSERT INTO product (id, image, product_id) values(?,?,?)";
+  pdo_execute($sql, $id, $image , $product_id);
+}
+//sửa
+function product_update($id,$image,$product_id)
+{
+  $sql = "UPDATE product_image SET id = ?, image = ? where product_id = ?";
+  pdo_execute($sql, $id, $image, $product_id);
+}
+//xoá
+function product_delete($id)
+{
+  $sql = "DELETE FROM product_image WHERE id = ?";
+  if (is_array($id)) {
+    foreach ($id as $item) {
+      pdo_execute($sql, $item);
+    }
+  }
+  pdo_execute($sql, $id);
+}
---
+++ dao/recipients.php
@@ -0,0 +1,36 @@
+<?php
+// lấy nhiều
+function recipients_select_all()
+{
+  $sql = "SELECT * FROM recipients";
+  return  pdo_query($sql);
+}
+//lấy 1
+function recipients_select_by_id($id)
+{
+  $sql = "SELECT * FROM recipients Where id = ?";
+  return pdo_query_one($sql, $id);
+}
+//thêm
+function recipients_insert($name, $phone, $recipients_id, $user_id)
+{
+  $sql = "INSERT INTO recipients (name, phone,recipients_id, user_id) values(?,?,?,?,?)";
+  pdo_execute($sql,$name, $phone, $recipients_id, $user_id);
+}
+//sửa
+function recipients_update($id, $name, $phone, $recipients_id, $user_id)
+{
+  $sql = "UPDATE recipients SET name = ?, price = ?, quantity = ?, featured = ?, active = ?, description = ?, category_id = ?, voucher_id = ?, brand_id = ? where id = ?";
+  pdo_execute($sql, $name, $phone, $recipients_id, $user_id, $id);
+}
+//xoá
+function recipients_delete($id)
+{
+  $sql = "DELETE FROM recipients WHERE id = ?";
+  if (is_array($id)) {
+    foreach ($id as $item) {
+      pdo_execute($sql, $item);
+    }
+  }
+  pdo_execute($sql, $id);
+}
