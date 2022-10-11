f874c36e3796af2c147bbd18f6d965cd465cb8db
duyynguyenn <114343968+duyynguyenn@users.noreply.github.com>
2022-10-11 16:21:55 +0700

    update add.php,menu.php

--- admin/loai-hang/add.php
+++ admin/loai-hang/add.php
@@ -1,2 +1,11 @@
 <h1 class="mx-[50px] text-[30px]">THÊM MỚI LOẠI HÀNG</h1>
-<a href="index.php?btn_list" class="hover:text-red-500 mx-[50px]">Danh sách</a>
+<form action="" method="post" enctype="multipart/form-data" class="mx-[50px]">
+<p class="mt-[15px]">Mã Loại</p><br>
+<input type="text" name="ma-loai" id="" class="bg-[#D9D9D9] rounded-[10px] w-full h-[50px]"><br>
+<p class="mt-[15px]">Tên Loại</p><br>
+<input type="text" name="ten-loai" id="" class="bg-[#D9D9D9] rounded-[10px] w-full h-[50px] mb-[15px]"><br>
+<div class="flex">
+    <input type="submit" name="submit" id="" value="Thêm mới" class="w-[100px] h-[50px] mr-[40px] bg-[#D9D9D9] rounded-[8px]">
+<input type="submit" name="nhap-lai" id="" value="Nhập lại" class="w-[100px] h-[50px] bg-[#D9D9D9] rounded-[8px] mr-[40px]">
+<a href="index.php?btn_list"><p class="w-[100px] py-[12px] px-[10px] bg-[#D9D9D9] rounded-[8px] mt-[2px]">Danh sách</p></a></div>
+</form>
