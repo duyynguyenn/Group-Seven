f4f46a528a220aa44272b66357a4ac3b2214872b
Ducvpop <105421466+Ducvpop@users.noreply.github.com>
2022-10-14 01:38:45 +0700

    duc
    site

---
+++ site/dangky.php
@@ -0,0 +1,64 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Sign Up</title>
+    <script src="https://cdn.tailwindcss.com/3.1.3"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
+</head>
+<body class="m-0 p-0 box-border"> <div class="mx-[10px] max-w-[1440px] mx-auto">
+    <section class="bg-[#253796] flex justify-around ">
+        <img class="py-[30px] " src="upload/img/logo.png" alt="">
+        <form class="bg-[#AE2B2B] flex h-[76px] w-[482px] rounded-[50px]  my-[25px]">
+            <input type="text" name="" id="" value="Tìm sản phẩm" class="pl-[37px] bg-[#AE2B2B] rounded-l-[50px] text-[32px] text-white border-r-2 border-black">
+            <button class="ml-[20px] w-[40px]"><img src="upload/icons/magnifying.svg" alt=""></button>
+        </form>
+        <div class="flex  pl-[30px]">
+            <a href="" class="flex">
+                <p class="text-[30px] text-white my-[30px] w-[91px] leading-[35px]">Giỏ hàng</p>
+                <img src="upload/icons/bag.svg" class="w-[65px]" alt="">
+            </a>
+            <a href="" class="">
+                <p class="my-[30px] ml-[50px] w-[198px] h-[65px] bg-[#FFFFFF] text-[32px] text-center pt-[5px]">Đăng nhập</p>
+            </a>
+        </div>
+        </section>
+        <section>
+            <ul class="flex text-[32px] justify-around border-black border-b-2 pb-[8px] pt-[8px]">
+                <li><a href="" class=" border-r-2 border-black  pr-[40px] pb-[12px] pt-[10px] hover:text-orange-400">Thực phẩm chức năng
+                </a></li>
+                <li><a href="" class="  pr-[120px] pb-[12px] border-r-2 border-black pt-[10px] hover:text-orange-400">Thuốc</a></li>
+                <li><a href="" class=" border-r-2 border-black pr-[120px] pb-[12px] pt-[10px] hover:text-orange-400">Bệnh</a></li>
+                <li><a href="" class=" pb-[10px] hover:text-orange-400" >Thông tin hỗ trợ</a></li>
+            </ul>
+        </section>
+        <section class="text-[40px] font-bold">
+            <h3 class="mt-[70px] mb-[70px] text-center">Đăng Ký hệ thống</h3>
+            <form action="" class="text-center">
+                <legend class="mb-[40px] mr-[690px]">HỌ VÀ TÊN</legend>
+                <input type="text" value="HỌ VÀ TÊN" class="bg-[#D9D9D9] w-[897px] h-[100px] pl-[60px] ">
+                <legend class="my-[40px] mr-[690px]">TÀI KHOẢN</legend>
+                <input type="text" value="TÀI KHOẢN" class="bg-[#D9D9D9] w-[897px] h-[100px] pl-[60px] ">
+                <legend class="my-[40px] mr-[690px]">MẬT KHẨU</legend>
+                <input type="text"  class="bg-[#D9D9D9] w-[897px] h-[100px] pl-[60px] ">
+                <legend class="my-[40px] mr-[490px]">NHẬP LẠI MẬT KHẨU</legend>
+                <input type="text"  class="bg-[#D9D9D9] w-[897px] h-[100px] pl-[60px] ">
+                <legend class="my-[40px] mr-[610px]">SỐ ĐIỆN THOẠI</legend>
+                <input type="text"  class="bg-[#D9D9D9] w-[897px] h-[100px] pl-[60px] ">
+                <legend class="my-[40px] mr-[790px]">EMAIL</legend>
+                <input type="text"  class="bg-[#D9D9D9] w-[897px] h-[100px] pl-[60px] ">
+                
+                <input type="submit" value="Đăng nhập" class="mt-[100px] bg-[#DF1A0D] w-[336px] h-[78px] rounded-[10px] text-[30px] mr-[560px] text-white">
+            </form>
+            <div class="flex space-x-2 ml-[268px] mt-[22px] mb-[70px]">
+                <p>Bạn đã có  tài khoản?</p>
+                <a href=""><p class="text-[#1A56CA]">Đăng nhập</p></a>
+            </div>
+        </section>
+
+</div>
+</body>
+</html>
