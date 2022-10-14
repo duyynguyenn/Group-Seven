56ba987ddb5170a3963642949b1e06dc245f411a
Ducvpop <105421466+Ducvpop@users.noreply.github.com>
2022-10-14 22:56:42 +0700

    upload img

--- site/layout.php
+++ site/layout.php
@@ -3,7 +3,6 @@ require_once '../global.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -15,15 +14,15 @@ require_once '../global.php';
 <body>
   <div class="mx-[10px] max-w-[1440px] mx-auto">
     <section class="bg-[#253796] flex justify-around ">
-    <img class="py-[30px] " src="<?= CONTENT_URL ?>/img/logo.png" alt="">
+    <img class="py-[30px] " src="<?= CONTENT_URL ?>/image/logo.png" alt="">
     <form class="bg-[#AE2B2B] flex h-[76px] w-[482px] rounded-[50px]  my-[25px]">
         <input type="text" name="" id="" value="Tìm sản phẩm" class="pl-[37px] bg-[#AE2B2B] rounded-l-[50px] text-[32px] text-white border-r-2 border-black">
-        <button class="ml-[20px] w-[40px]"><img src="<?= CONTENT_URL ?>/img/icons/magnifying.svg" alt=""></button>
+        <button class="ml-[20px] w-[40px]"><img src="<?= CONTENT_URL ?>/image/icons/magnifying.svg" alt=""></button>
     </form>
     <div class="flex  pl-[30px]">
         <a href="" class="flex">
             <p class="text-[30px] text-white my-[30px] w-[91px] leading-[35px]">Giỏ hàng</p>
-            <img src="<?= CONTENT_URL ?>/img/icons/bag.svg" class="w-[65px]" alt="">
+            <img src="<?= CONTENT_URL ?>/image/icons/bag.svg" class="w-[65px]" alt="">
         </a>
         <a href="" class="">
             <p class="my-[30px] ml-[50px] w-[198px] h-[65px] bg-[#FFFFFF] text-[32px] text-center pt-[5px]">Đăng nhập</p>
