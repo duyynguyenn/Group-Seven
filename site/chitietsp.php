c8f859fba48fc517d7b0e9ed2e71b1425abad475
Ducvpop <105421466+Ducvpop@users.noreply.github.com>
2022-10-19 01:19:12 +0700

    Create chitietsp.php

---
+++ site/chitietsp.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <script src="https://cdn.tailwindcss.com/3.1.3"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;700&display=swap" rel="stylesheet">
+</head>
+<body class="m-0 p-0 box-border">
+    <div class="mx-[10px] max-w-[1440px] mx-auto">
+        <section class="bg-[#253796] flex justify-around ">
+        <img class="py-[30px] " src="image/logo.png" alt="">
+        <form class="bg-[#AE2B2B] flex h-[76px] w-[482px] rounded-[50px]  my-[25px]">
+            <input type="text" name="" id="" value="Tìm sản phẩm" class="pl-[37px] bg-[#AE2B2B] rounded-l-[50px] text-[32px] text-white border-r-2 border-black">
+            <button class="ml-[20px] w-[40px]"><img src="image/icons/magnifying.svg" alt=""></button>
+        </form>
+        <div class="flex  pl-[30px]">
+            <a href="" class="flex">
+                <p class="text-[30px] text-white my-[30px] w-[91px] leading-[35px]">Giỏ hàng</p>
+                <img src="image/icons/bag.svg" class="w-[65px]" alt="">
+            </a>
+            <a href="" class="">
+                <p class="my-[30px] ml-[50px] w-[198px] h-[65px] bg-[#FFFFFF] text-[32px] text-center pt-[5px]">Đăng nhập</p>
+            </a>
+        </div>
+        </section>
+
+        <section>
+            <ul class="flex text-[32px] justify-around  ">
+                <li><a href="Trangchu.html" class=" border-r-2 border-black pr-[40px] pb-[10px]">Thực phẩm chức năng
+                </a></li>
+                <li><a href="ChitietSP.html" class="  pr-[120px] pb-[10px] border-r-2 border-black">Thuốc</a></li>
+                <li><a href="Hoidap.html" class=" border-r-2 border-black pr-[120px] pb-[10px]">Bệnh</a></li>
+                <li><a href="" class=" pb-[10px]" >Thông tin hỗ trợ</a></li>
+            </ul>
+        </section>
+        <section class="mt-[10px]">
+            <img src="image/banner.png" alt="">
+        </section>
+        <section>
+            <div class="grid grid-cols-2 gap-[93px]">
+                <div>
+                    <img class="ml-[164px] mt-[200px] mb-[28px]" src="image/anh1.png" alt="">
+                    <div class="flex">
+                        <img class=" h-[226px]" src="image/anh2.png" alt="">
+                        <img class=" h-[226px]" src="image/anh3.png" alt="">
+                        <img class=" h-[226px]" src="image/anh4.png" alt="">
+                    </div>
+                </div>
+                <div class="mt-[23px]">
+                    <p class="text-[24px] font-medium ">Thương hiệu: <span class="text-blue-800">Optimax</span> </p>
+                    <p class="text-[36px] font-medium my-[12px]" >Viên Sủi Optimax Immunity Booster Vid - Fighter Tăng Sức Đề Kháng 20 Viên</p>
+                    <h3 class="text-[40px] font-bold">115.000đ /Hộp</h3>
+                    <p class="text-[24px] font-medium  w-[473px]">Danh mục:  Tăng sức đề kháng, miễn dịch<br>
+                        Dạng bào chế:  Viên sủi<br>
+                        Quy cách:  Tuýp 20 viên<br>
+                        Xuất xứ thương hiệu:  Mỹ<br>
+                        Nhà sản xuất:  Invapharm., Inc<br>
+                        Nước sản xuất:  Hoa Kỳ<br>
+                        Công dụng:  Optimax Immunity Booster Vid-Fighter giúp bổ sung vitamin C, vitamin D, kẽm và mangan cho cơ thể, giúp tăng cường sức đề kháng.
+                    </p>
+                    <p class="text-[24px] font-bold my-[22px]">Chọn số lượng:</p>
+                    <form class="text-white text-[30px] font-bold">
+                        <button type="submit" class=" bg-[#302887] rounded-[40px] w-[359px] py-[8px] mr-[14px]">Thêm vào giỏ hàng</button>
+                        <button type="submit" class="bg-[#DFA40D] rounded-[40px] w-[200px] py-[8px]">Mua</button>
+                    </form>
+                </div>
+            </div>
+        </section>
+        <section class="mt-[27px] ml-[79px] mr-[46px]">
+            <div class="flex mb-[18px]">
+                <div class="">
+                    <img src="image/ngoi-sao.png" alt="">
+                </div>
+                <p class="text-[32px] font-bold ml-[21px] mt-[15px]">Sản phẩm liên quan</p>
+            </div>
+            <div class="grid grid-cols-3 gap-[56px] mb-[80px]">
+                <div class="bg-[#F7EEEE] block text-center font-bold rounded-[20px]">
+                    <img src="image/menu1.png" class="w-[225px] mx-[85px] mt-[17px]">
+                    <p class="text-[20px] my-[20px]">Viên uống Trùng Thảo Gold giúp ngủ ngon thành công</p>
+                    <button type="submit" class="bg-[#DFA40D] rounded-[40px] text-[30px] w-[180px] py-[10px] mb-[26px]">Mua</button>
+                </div>
+                <div class="bg-[#F7EEEE] block text-center font-bold rounded-[20px]">
+                    <img src="image/menu1.png" class="w-[225px] mx-[85px] mt-[17px]">
+                    <p class="text-[20px] my-[20px]">Viên uống Trùng Thảo Gold giúp ngủ ngon thành công</p>
+                    <button type="submit" class="bg-[#DFA40D] rounded-[40px] text-[30px] w-[180px] py-[10px] mb-[26px]">Mua</button>
+                </div>
+                <div class="bg-[#F7EEEE] block text-center font-bold rounded-[20px]">
+                    <img src="image/menu1.png" class="w-[225px] mx-[85px] mt-[17px]">
+                    <p class="text-[20px] my-[20px]">Viên uống Trùng Thảo Gold giúp ngủ ngon thành công</p>
+                    <button type="submit" class="bg-[#DFA40D] rounded-[40px] text-[30px] w-[180px] py-[10px] mb-[26px]">Mua</button>
+                </div>
+            </div>
+        </section>
+        <section class="mx-[85px]">
+            <p class="w-full py-[30px] pl-[51px] bg-[#F1EBEB] text-[24px] font-bold rounded-[10px] mb-[25px]">Bình luận</p>
+            <form class="mx-[15px] grid grid-cols-2 gap-[540px] font-bold text-[32px]">
+                <input type="text" value="Nhập nội dung:" class="text-black mx-auto ">
+                <input type="submit" value="Gửi bình luận" class="bg-[#4158A9] py-[13px] w-[325px] text-center text-white rounded-[50px]">
+            </form>
+            <textarea class="bg-[#F1EBEB] rounded-[50px] mt-[39px] mb-[54px]" name="" id="" cols="163" rows="11"></textarea>
+        </section>
+        <section class="bg-[#D9D9D9] text-[12px] font-bold">
+            <div class="flex justify-around py-[50px]">
+                <div class="block leading-[40px]">
+                    <ul><li><a href="">Về chúng tôi</a></li>
+                        <li><a href="">Thuốc</a></li>
+                        <li><a href="">Bệnh</a></li></ul>
+                </div>
+                <div class="block leading-[40px]">
+                    <ul>
+                        <li><a href="">Bảo vệ sức khỏe</a></li>
+                        <li><a href="">Trang thiết bị y tế</a></li>
+                        <li><a href="">Góc sức khỏe</a></li>
+                    </ul>
+                </div>
+                <div class="block leading-[40px]">
+                    <ul>
+                        <li><a href="">Vitamin</a></li>
+                        <li><a href="">Hỗ trợ điều trị</a></li>
+                        <li><a href="">Cải thiện tăng sức đề kháng</a></li>
+                    </ul>
+                </div>
+                <div class="bg-[#DC1212] w-[271px] h-[135px] rounded-[20px]">
+                    
+                    <a href=""><p class="leading-[90px] text-white text-center mb-[20px]">Tải ứng dụng mua thuốc trực tuyến</p></a>
+                    <div class="flex space-x-7 -mt-[35px] ml-[10px]">
+                        <img src="image/ap.png" alt="">
+                        <img src="image/gg.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+    </div>
+    
+</body>
+</html>
