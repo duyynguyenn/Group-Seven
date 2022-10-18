6bbcda09826528f8963e62ac31b5fd3d98a4381f
duyynguyenn <114343968+duyynguyenn@users.noreply.github.com>
2022-10-18 12:33:29 +0700

    update hang-hoa

---
+++ admin/hang-hoa/add.php
@@ -0,0 +1,58 @@
+<h2 class="bg-[#85CC73] text-[#24432B] mx-[150px] text-[36px] py-[33px] pl-[20px] mb-[45px]">THÊM MỚI HÀNG HÓA
+        </h2>
+        <form action="">
+            <div class="mx-[150px] grid grid-cols-3 gap-[100px]">
+                <div>
+                    <p class="text-[36px] mb-[15px]">MÃ HÀNG HÓA</p>
+                    <input type="text" name="" id="" value="AUTO NUMBER" class="py-[18px] pl-[30px] w-[320px] bg-[#D9D9D9]">
+                </div>
+                <div>
+                    <p class="text-[36px] mb-[15px]">TÊN HÀNG HÓA</p>
+                    <input type="text" name="" id="" class="py-[18px] pl-[30px] w-[320px] bg-[#D9D9D9]">
+                </div>
+                <div>
+                    <p class="text-[36px] mb-[15px]">ĐƠN GIÁ</p>
+                    <input type="text" name="" id="" class="py-[18px] pl-[30px] w-[320px] bg-[#D9D9D9]">
+                </div>
+            </div>
+    
+            <div class="mx-[150px] grid grid-cols-3 gap-[100px] my-[30px]">
+                <div>
+                    <p class="text-[36px] mb-[15px]">GIẢM GIÁ</p>
+                    <input type="text" name="" id="" class="py-[18px] pl-[30px] w-[320px] bg-[#D9D9D9]">
+                </div>
+                <div>
+                    <p class="text-[36px] mb-[15px]">HÌNH ẢNH</p>
+                    <input type="file" name="" id="" class="py-[18px] pl-[30px] w-[320px] bg-[#D9D9D9]">
+                </div>
+                <div>
+                    <p class="text-[36px] mb-[15px]">LOẠI HÀNG</p>
+                    <input type="text" name="" id="" class="py-[18px] pl-[30px] w-[320px] bg-[#D9D9D9]">
+                </div>
+            </div>
+    
+            <div class="mx-[150px] grid grid-cols-3 gap-[100px]">
+                <div>
+                    <p class="text-[36px] mb-[15px]">HÀNG ĐẶC BIỆT</p>
+                    <input type="text" name="" id="" class="py-[18px] pl-[30px] w-[320px] bg-[#D9D9D9]">
+                </div>
+                <div>
+                    <p class="text-[36px] mb-[15px]">NGÀY NHẬP</p>
+                    <input type="text" name="" id="" class="py-[18px] pl-[30px] w-[320px] bg-[#D9D9D9]">
+                </div>
+                <div>
+                    <p class="text-[36px] mb-[15px]">SỐ LƯỢNG XEM</p>
+                    <input type="text" name="" id="" class="py-[18px] pl-[30px] w-[320px] bg-[#D9D9D9]">
+                </div>
+            </div>
+    
+            <div class="mx-[150px] mt-[30px]">
+                <h2 class="text-[36px] mb-[15px]">MÔ TẢ</h2>
+                <textarea class="bg-[#D9D9D9]" name="" id="" cols="147" rows="6"></textarea>
+            </div>
+            <div class="flex mx-[150px] mt-[15px] text-[30px] space-x-5 mb-[100px]">
+                <button type="submit" class="bg-[#E0D0D0] py-[10px] px-[23px] text-black rounded-[10px]">Thêm mới</button>
+                <button type="submit" class="bg-[#E0D0D0] py-[10px] px-[23px] text-black rounded-[10px]">Nhập lại</button>
+                <button type="submit" class="bg-[#E0D0D0] py-[10px] px-[23px] text-black rounded-[10px]">Danh sách</button>
+            </div>
+        </form>
---
+++ admin/hang-hoa/index.php
@@ -0,0 +1,5 @@
+<?php
+// $rows = products_select_all();
+$VIEW_NAME = 'add.php';
+include_once '../layout.php';
+?>
