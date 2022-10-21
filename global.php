4d3544dfa0b5a6f2dd34cf4ddbcb2e0cea866229
khoadv25 <khoadvph24224@fpt.edu.vn>
2022-10-21 12:01:04 +0700

    update hiển thị site + sản phẩm

--- global.php
+++ global.php
@@ -1,10 +1,13 @@
 
+
+
+<?php
+
 const BASE_URL = "http://localhost:81/nhom7-du-an-mau/";
 const SITE_URL = BASE_URL . "site/index.php";
 const CONTENT_URL = BASE_URL . "content/";
 const ADMIN_URL = BASE_URL ."admin/";
 
-<?php
 function pdo_get_connection(){
 $servername = "localhost";
 $username = "root";
---
+++ site/home.php
@@ -0,0 +1,21 @@
+<div class=" flex justify-center list-none ">
+
+    <div class=" ">
+        <div class=" text-center py-4 pb-4 font-bold text-2xl text-red-600 ">
+            <p>Thịnh Hành</p>
+        </div>
+
+        <div class="grid grid-cols-4 gap-32 flex justify-center ">
+            <?php  foreach($products as $value) :  ?>
+            <div>
+                <div class="py-4">
+                    <img src="<?= CONTENT_URL ?>/image/ảnh thuốc siro,forte-mom 1.png" alt="">
+                </div>
+                <a href="" class="text-center text-base font-bold  ">
+                    <li> <?php echo $value["name"] ?></li>
+                    <li class="py-2 text-red-600"><?php echo $value["price"] ?></li>
+                </a>
+            </div>
+            <?php  endforeach  ?>
+        </div>
+    </div>
