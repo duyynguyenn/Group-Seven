06274c592596bc06ef25316ee381db446b667c81
khoadv25 <khoadvph24224@fpt.edu.vn>
2022-10-22 10:18:33 +0700

    Update layout.php

--- site/layout.php
+++ site/layout.php
@@ -51,6 +51,39 @@ require_once '../global.php';
 
 
   </div>
+
+  <section class="bg-[#D9D9D9] text-[12px] font-bold">
+            <div class="flex justify-around py-[50px]">
+                <div class="block leading-[40px]">
+                    <ul><li><a href="">Về chúng tôi</a></li>
+                        <li><a href="">Thuốc</a></li>
+                        <li><a href="">Bệnh</a></li></ul>
+                </div>
+                <div class="block leading-[40px]">
+                    <ul>
+                        <li><a href="">Bảo vệ sức khỏe</a></li>
+                        <li><a href="">Trang thiết bị y tế</a></li>
+                        <li><a href="">Góc sức khỏe</a></li>
+                    </ul>
+                </div>
+                <div class="block leading-[40px]">
+                    <ul>
+                        <li><a href="">Vitamin</a></li>
+                        <li><a href="">Hỗ trợ điều trị</a></li>
+                        <li><a href="">Cải thiện tăng sức đề kháng</a></li>
+                    </ul>
+                </div>
+                <div class="bg-[#DC1212] w-[271px] h-[135px] rounded-[20px]">
+                    
+                    <a href=""><p class="leading-[90px] text-white text-center mb-[20px]">Tải ứng dụng mua thuốc trực tuyến</p></a>
+                    <div class="flex space-x-7 -mt-[35px] ml-[10px]">
+                        <img src="image/ap.png" alt="">
+                        <img src="image/gg.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </section>
+
 </body>
 
 </html>
