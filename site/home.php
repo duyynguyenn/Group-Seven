8f76ddc53ee2859b1411ae43543f624c649b4632
duyynguyenn <114343968+duyynguyenn@users.noreply.github.com>
2022-10-23 17:16:06 +0700

    chỉnh layout

--- site/home.php
+++ site/home.php
@@ -2,20 +2,20 @@
 
 <div class=" flex justify-center list-none ">
 
-    <div class=" ">
-        <div class=" text-center py-4 pb-4 font-bold text-2xl text-red-600 ">
-            <p>Thịnh Hành</p>
+    <div class="">
+        <div class="text-center my-[40px] font-bold text-2xl text-red-600 ">
+            <p class="text-[35px]">Thịnh Hành</p>
         </div>
 
         <div class="grid grid-cols-4 gap-32 flex justify-center ">
             <?php  foreach($products as $value) :  ?>
             <div>
                 <div class="py-4">
-                    <img src="<?= $value['image'] ?>" alt="">
+                    <a href=""><img src="<?= $value['image'] ?>" alt=""></a>
                 </div>
                 <a href="" class="text-center text-base font-bold  ">
                     <li> <?php echo $value["name"] ?></li>
-                    <li class="py-2 text-red-600"><?php echo $value["price"] ?></li>
+                    <li class="py-2 text-red-600"><?php echo $value["price"] ?>đ</li>
                 </a>
             </div>
             <?php  endforeach  ?>
