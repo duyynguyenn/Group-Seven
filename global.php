bb19f466582559f46f1557d3f98cb54e8c0d59bd
duyynguyenn <114343968+duyynguyenn@users.noreply.github.com>
2022-10-23 23:06:33 +0700

    Update global.php

--- global.php
+++ global.php
@@ -1,6 +1,6 @@
 <?php
 
-const BASE_URL = "http://localhost:81/nhom7-du-an-mau/";
+const BASE_URL = "http://localhost:/nhom7-du-an-mau/";
 const SITE_URL = BASE_URL . "site/index.php";
 const CONTENT_URL = BASE_URL . "content/";
 const ADMIN_URL = BASE_URL ."admin/";
